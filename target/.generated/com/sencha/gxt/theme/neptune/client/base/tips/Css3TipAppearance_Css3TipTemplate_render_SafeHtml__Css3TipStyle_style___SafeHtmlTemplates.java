a4ff87a70768936111fb0c38bc2faf90fcda1d12
opeter <peter.okolicsanyi@infocompass.hu>
2017-08-04 23:05:52 +0200

    remove useless files

--- target/.generated/com/sencha/gxt/theme/neptune/client/base/tips/Css3TipAppearance_Css3TipTemplate_render_SafeHtml__Css3TipStyle_style___SafeHtmlTemplates.java
+++
@@ -1,9 +0,0 @@
-package com.sencha.gxt.theme.neptune.client.base.tips;
-
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.client.SafeHtmlTemplates.Template;
-
-public interface Css3TipAppearance_Css3TipTemplate_render_SafeHtml__Css3TipStyle_style___SafeHtmlTemplates extends com.google.gwt.safehtml.client.SafeHtmlTemplates {
-  @Template("<div class='{0}'><div class='{1}'><div class='{2}'></div><div class='{3}'><span class='{4}'></span></div><div class='{5}'><span class='{6}'></span></div></div></div>")
-  SafeHtml render0(java.lang.String arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3, java.lang.String arg4, java.lang.String arg5, java.lang.String arg6);
-}
--- target/.generated/com/sencha/gxt/theme/neptune/client/sliced/mask/SlicedMaskAppearance_SlicedMaskTemplate_template_SafeHtml__SlicedMaskStyles_style__String_message___SafeHtmlTemplates.java
+++
@@ -1,9 +0,0 @@
-package com.sencha.gxt.theme.neptune.client.sliced.mask;
-
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.client.SafeHtmlTemplates.Template;
-
-public interface SlicedMaskAppearance_SlicedMaskTemplate_template_SafeHtml__SlicedMaskStyles_style__String_message___SafeHtmlTemplates extends com.google.gwt.safehtml.client.SafeHtmlTemplates {
-  @Template("<div class=\"{0}\">{1}</div>")
-  SafeHtml template0(java.lang.String arg0, java.lang.String arg1);
-}
